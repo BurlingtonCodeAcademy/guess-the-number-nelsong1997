646f9a270f953a9223f26818e11d7d42b77675cc
Gabe N <nelsong1997@gmail.com>
2019-06-07 13:45:18 -0400

    version 2 custom ranges

--- index.js
+++ index.js
@@ -10,19 +10,79 @@ function ask(questionText) {
 start();
 
 async function start() {
-  console.log('Salutations, honored guest! Please think of an integer between 1 and 100, inclusive.\nI will now attempt to guess your number.')
-    let guess = 50;
-    let cutMeInHalf = 50;
-    let tries = 1;
-    let i = 0;
-    let cheat = 0;
+  let j = 0;
+  let min = 0;
+  let max = 0;
+  console.log('Salutations, honored guest! Let\'s play a game; you\'ll think of an integer number, and I\'ll try to guess it.');
+  let response = await ask('First, let\'s decide on a range of numbers. What is the lowest number I should be guessing?\n');
+  if (Number(response).toString()!=='NaN' && (Number(response))%1===0) {
+    min = Number(response);
+  } else if (Number(response).toString()!=='NaN' && (Number(response))%1!==0) {
+    j = 1;
+  } else {
+    j = 2;
+  }
+  while (j > 0) {
+    if (j===1) {
+      response = await ask('Whoops! That\'s not an integer. No decimals or fractions please! Enter a new minimum.\n');
+    } else if (j===2) {
+      response = await ask('Whoops! That\'s not a number that I can recognize. Please enter a number as a minimum!\n');
+    } else {
+      console.log('Fatal error: unhandled exception :(');
+      process.exit();
+    }
+    if (Number(response).toString()!=='NaN' && (Number(response))%1===0) {
+      min = Number(response);
+      j = 0;
+    } else if (Number(response).toString()!=='NaN' && (Number(response))%1!==0) {
+      j = 1;
+    } else {
+      j = 2;
+    }
+  }
+  j = 0;
+  response = await ask('Okay, got it! What is the highest number I should be guessing?\n');
+  if (Number(response).toString()!=='NaN' && (Number(response))%1===0 && response >= min) {
+    max = Number(response);
+  } else if (Number(response).toString()!=='NaN' && (Number(response))%1!==0) {
+    j = 1;
+  } else if (response < min) {
+    j = 2;
+  } else {
+    j = 3;
+  }
+  while (j > 0) {
+    if (j===1) {
+      response = await ask('Whoops! That\'s not an integer. No decimals or fractions please! Enter a new maximum.\n');
+    } else if (j===2) {
+      response = await ask('Whoops! The maximum you just chose is less than the minimum you chose. Please enter a new maximum.\n')
+    } else if (j===3) {
+      response = await ask('Whoops! That\'s not a number that I can recognize. Please enter a number as a maximum!\n');
+    } else {
+      console.log('Fatal error: unhandled exception :(');
+      process.exit();
+    }
+    if (Number(response).toString()!=='NaN' && (Number(response))%1===0 && response >= min) {
+      max = Number(response);
+      j = 0;
+    } else if (Number(response).toString()!=='NaN' && (Number(response))%1!==0) {
+      j = 1;
+    } else if (response < min) {
+      j = 2;
+    } else {
+      j = 3;
+    }
+  }
+  console.log('Awesome! I will now attempt to guess your number, which is between ' + min + ' and ' + max + '.');
+  let cutMeInHalf = (max - min)/2;
+  let guess = Math.round(cutMeInHalf) + min;
+  let tries = 1;
+  let i = 0;
     while (i===0) {
-       let response = await ask('Is your number... ' + guess + '? (Y=Yes; L=No, my number is lower; H=No, my number is higher)\n');
+       response = await ask('Is your number... ' + guess + '? (Y=Yes; L=No, my number is lower; H=No, my number is higher)\n');
        if (response=='y' || response=='Y') {
             if (tries===1) {
                 console.log('Wow! I guessed your number in just one try. See ya! B^)');
-            } else if (cheat===1) {
-                console.log('Hmmm... I guessed your number in ' + tries + ' tries. It would\'ve taken me less tries if you hadn\'t cheated!!');
             } else {
                 console.log('Yay! I guessed your number in ' + tries + ' tries. Bye!');
             }
@@ -48,17 +108,9 @@ async function start() {
                 addMe = 1;
             }
             guess = guess + addMe;
-            if (guess > 100) {
-                console.log('Hey! Your number can\'t be more than 100! Next time, play without cheating!!!!');
-                process.exit();
-            }
         } else {
             console.log('ERROR: Bad input. It\'s okay, just try again!')
-        }
-        if (tries===8) {
-            console.log('Mathematically speaking, one or more of your answers so far has been incorrect. Continue if you wish...');
-            cheat = 1;
-    }
+            }
   }
   process.exit();
 }
