60d4ee5fcc3b1da56a70b892003282c3d081a5be
Gabe N <nelsong1997@gmail.com>
2019-06-07 09:18:31 -0400

    version 1

--- index.js
+++ index.js
@@ -10,9 +10,55 @@ function ask(questionText) {
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+  console.log('Salutations, honored guest! Please think of an integer between 1 and 100, inclusive.\nI will now attempt to guess your number.')
+    let guess = 50;
+    let cutMeInHalf = 50;
+    let tries = 1;
+    let i = 0;
+    let cheat = 0;
+    while (i===0) {
+       let response = await ask('Is your number... ' + guess + '? (Y=Yes; L=No, my number is lower; H=No, my number is higher)\n');
+       if (response=='y' || response=='Y') {
+            if (tries===1) {
+                console.log('Wow! I guessed your number in just one try. See ya! B^)');
+            } else if (cheat===1) {
+                console.log('Hmmm... I guessed your number in ' + tries + ' tries. It would\'ve taken me less tries if you hadn\'t cheated!!');
+            } else {
+                console.log('Yay! I guessed your number in ' + tries + ' tries. Bye!');
+            }
+            i = 1;
+            process.exit();
+        } else if (response=='L' || response=='l') {
+            tries = tries + 1;
+            cutMeInHalf = cutMeInHalf/2;
+            addMe = Math.round(cutMeInHalf);
+            if (addMe < 1) {
+                addMe = 1;
+            }
+            guess = guess - addMe;
+            if (guess < 1) {
+                console.log('Hey! Your number can\'t be less than 1! Next time, play without cheating!!!!');
+                process.exit();
+            }
+        } else if (response=='H' || response=='h')  {
+            tries = tries + 1;
+            cutMeInHalf = cutMeInHalf/2;
+            addMe = Math.round(cutMeInHalf);
+            if (addMe < 1) {
+                addMe = 1;
+            }
+            guess = guess + addMe;
+            if (guess > 100) {
+                console.log('Hey! Your number can\'t be more than 100! Next time, play without cheating!!!!');
+                process.exit();
+            }
+        } else {
+            console.log('ERROR: Bad input. It\'s okay, just try again!')
+        }
+        if (tries===8) {
+            console.log('Mathematically speaking, one or more of your answers so far has been incorrect. Continue if you wish...');
+            cheat = 1;
+    }
+  }
   process.exit();
 }
